4765aed67e8be53f7859c871bf70858fb389b4ef
Anheurystics <willard.torres1008@gmail.com>
2016-06-01 10:12:54 +0800

    Add more color conversion functions

--- src/ferret/Color.hx
+++ src/ferret/Color.hx
@@ -14,5 +14,41 @@ class Color
 		return c;
 	}
 	
+	public static function fromInt(r: Int, g: Int, b: Int, a: Int = 255): Color
+	{
+		return fromFloat(r / 255.0, g / 255.0, b / 255.0, a / 255.0);
+	}
+	
+	public static function fromHexInt32(rgba: Int): Color
+	{
+		var a: Int = a & 0xFF;
+		var b: Int = (a >> 8) & 0xFF;
+		var g: Int = (a >> 16) & 0xFF;
+		var r: Int = (a >> 24) & 0xFF;
+		
+		return fromInt(r, g, b, a);
+	}
+	
+	public static function fromHexInt24(rgb: Int): Color
+	{
+		var b: Int = a & 0xFF;
+		var g: Int = (a >> 8) & 0xFF;
+		var r: Int = (a >> 16) & 0xFF;
+		
+		return fromInt(r, g, b, 255);
+	}
+	
+	public static function fromHexString(hexString: String): Color
+	{
+		if (hexString.indexOf("0x") != 0)
+			return null;
+		if (hexString.length == 8)
+			return fromHexInt24(Std.parseInt(hexString));
+		if (hexString.length == 10)
+			return fromHexInt32(Std.parseInt(hexString));
+		
+		return null;
+	}
+	
 	private function new() {}
 }
