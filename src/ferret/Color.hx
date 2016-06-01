f2fb6c1bda093e15ea6f3d2aec56f15cf7e17e6a
Anheurystics <willard.torres1008@gmail.com>
2016-06-01 10:16:57 +0800

    Add more Color functions, fix typos

--- src/ferret/Color.hx
+++ src/ferret/Color.hx
@@ -21,19 +21,19 @@ class Color
 	
 	public static function fromHexInt32(rgba: Int): Color
 	{
-		var a: Int = a & 0xFF;
-		var b: Int = (a >> 8) & 0xFF;
-		var g: Int = (a >> 16) & 0xFF;
-		var r: Int = (a >> 24) & 0xFF;
+		var a: Int = rgba & 0xFF;
+		var b: Int = (rgba >> 8) & 0xFF;
+		var g: Int = (rgba >> 16) & 0xFF;
+		var r: Int = (rgba >> 24) & 0xFF;
 		
 		return fromInt(r, g, b, a);
 	}
 	
 	public static function fromHexInt24(rgb: Int): Color
 	{
-		var b: Int = a & 0xFF;
-		var g: Int = (a >> 8) & 0xFF;
-		var r: Int = (a >> 16) & 0xFF;
+		var b: Int = rgb & 0xFF;
+		var g: Int = (rgb >> 8) & 0xFF;
+		var r: Int = (rgb >> 16) & 0xFF;
 		
 		return fromInt(r, g, b, 255);
 	}
@@ -48,6 +48,25 @@ class Color
 			return fromHexInt32(Std.parseInt(hexString));
 		
 		return null;
+	}
+	
+	public function toHex24(): Int
+	{
+		var ir: Int = Std.int(r * 255);
+		var ig: Int = Std.int(g * 255);
+		var ib: Int = Std.int(b * 255);
+
+		return (ir << 16) | (ig << 8) | ib;
+	}
+	
+	public function toHex32(): Int
+	{
+		var ir: Int = Std.int(r * 255);
+		var ig: Int = Std.int(g * 255);
+		var ib: Int = Std.int(b * 255);
+		var ia: Int = Std.int(a * 255);
+		
+		return (ir << 24) | (ig << 16) | (ib << 8) | ia;
 	}
 	
 	private function new() {}
