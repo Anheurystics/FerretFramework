2c77e777a4f29d3678d06692dac63715ad0fc509
Anheurystics <willard.torres1008@gmail.com>
2016-06-08 08:36:16 +0800

    Implement basic emitting for the particle system

--- src/ferret/ParticleSystem.hx
+++ src/ferret/ParticleSystem.hx
@@ -1,8 +1,8 @@
 package ferret;
+import ferret.ParticleSystem.Particle;
+import ferret.SpriteBatch.Sprite;
 
 typedef Particle =  {
-	> SpriteBatch.Sprite,
-	
 	var life: Float;
 	var lifetime: Float;
 	var vx: Float;
@@ -11,24 +11,47 @@ typedef Particle =  {
 
 class ParticleSystem extends SpriteBatch
 {
-	public function new(_sheet: TextureSheet) 
+	public var x: Float;
+	public var y: Float;
+	
+	private var particleData: Array<Particle> = new Array();
+	
+	public function new(_x: Float = 0, _y: Float = 0, _sheet: TextureSheet) 
 	{
 		super(_sheet);
+		
+		x = _x;
+		y = _y;
 	}
 	
 	public function update(delta: Float): Void
 	{
-		for (sprite in sprites)
+		for (i in 0...sprites.length)
 		{
-			if (Std.is(sprite, Particle))
-			{
-				sprite.x += vx * 
-			}
+			var sprite = sprites[i];
+			sprite.x += particleData[i].vx * delta;
+			sprite.y += particleData[i].vy * delta;
+					
+			updateSprite(i);				
 		}
 	}
 	
 	public function emit(n: Int): Void
 	{
-		
+		for (i in 0...n)
+		{
+			var angle = Math.random() * Math.PI;
+			
+			var newParticle: Particle = {
+				life: 1.0, lifetime: 0.0, vx: 32.0 * Math.cos(angle), vy: 32.0 * Math.sin(angle)
+			};
+			
+			var newSprite: Sprite = {
+				name: "particle", x: x, y: y, scaleX: 1, scaleY: 1, rotation: 0
+			}
+			
+			particleData.push(newParticle);
+			sprites.push(newSprite);
+		}
 	}
 }
