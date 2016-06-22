c326ff6ccff39cdcb6fc023af7a8610e78702ca1
Anheurystics <willard.torres1008@gmail.com>
2016-06-22 13:25:30 +0800

    Add renderTypes to Scene3D

--- ferret/Scene3D.hx
+++ ferret/Scene3D.hx
@@ -14,18 +14,41 @@ typedef Instance3D =
 
 class Scene3D extends Scene
 {
+	private static inline var UNTEXTURED: Int = 1;
+	private static inline var TEXTURED: Int = 2;
+	
 	private var untextured3D: Program = new Program(Assets.getText("shaders/untextured3D.vert"), Assets.getText("shaders/untextured3D.frag"));
-	private var textured3D: Program= new Program(Assets.getText("shaders/textured3D.vert"), Assets.getText("shaders/textured3D.frag"));	
+	private var textured3D: Program = new Program(Assets.getText("shaders/textured3D.vert"), Assets.getText("shaders/textured3D.frag"));	
 	
 	private var meshList: Array<Mesh> = new Array();
 	private var modelInstances: Map<Int, Array<Transform3D>> = new Map();
+	
+	private var renderType: Int;
 	
 	private var perspCam: PerspectiveCamera;
 	
 	public function new() 
 	{
 		super();
+		
+		setRenderType(UNTEXTURED);
 	}
+	
+	public function setRenderType(type: Int)
+	{
+		switch(type)
+		{
+			case UNTEXTURED:
+				program = untextured3D;
+			case TEXTURED:
+				program = textured3D;
+			default:
+				program = textured3D;
+		}
+		
+		renderType = type;
+		renderer.uploadProgram(program);
+	}	
 	
 	public function createModelInstance(mesh: Mesh): Transform3D
 	{
